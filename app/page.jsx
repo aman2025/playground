0f690c439cbb06775a4e26460f928a9c8eaf3a88
aman2025 <2247367564@qq.com>
2024-11-04 10:11:08 +0800

    page.js 删除原来main区域图片和文字

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,6 @@ import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 export default async function Home() {
   // Fetch session data
   const session = await getServerSession(authOptions)
-  console.log('session21122:', session)
 
   if (!session) {
     redirect('/signin')
@@ -14,11 +13,6 @@ export default async function Home() {
 
   return (
     // Pass session as a prop to Layout
-    <Layout session={session}>
-      <div className="flex h-full flex-col items-center justify-center">
-        <Image src="/images/logo.png" alt="Playground Logo" width={80} height={80} />
-        <h1 className="mt-4 text-2xl text-gray-400">111PLAYGROUND</h1>
-      </div>
-    </Layout>
+    <Layout session={session}></Layout>
   )
 }
