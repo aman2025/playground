2e2fba50ad55320978166d3474a25589048db083
aman2025 <2247367564@qq.com>
2024-12-12 17:11:53 +0800

    调整消息页面，骨架屏样式，宽度100%，高h-5

--- components/ChatInterface.jsx
+++ components/ChatInterface.jsx
@@ -121,8 +121,9 @@ export default function ChatInterface({ session }) {
         <div key={i} className="flex items-start space-x-4">
           <Skeleton className="h-10 w-10 rounded-full" />
           <div className="flex-1 space-y-2">
-            <Skeleton className="h-4 w-[250px]" />
-            <Skeleton className="h-4 w-[400px]" />
+            <Skeleton className="h-5 w-[360px]" />
+            <Skeleton className="h-5 w-full" />
+            <Skeleton className="h-5 w-full" />
           </div>
         </div>
       ))}
