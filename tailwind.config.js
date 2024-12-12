28cc2f766e00d327cc9030de56855d733382453d
aman2025 <2247367564@qq.com>
2024-12-12 11:39:17 +0800

    tailwind.config.js删除多余fade-in，zoom-in动画样式

--- tailwind.config.js
+++ tailwind.config.js
@@ -65,20 +65,10 @@ module.exports = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: 0 },
         },
-        'fade-in': {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
-        },
-        'zoom-in': {
-          '0%': { transform: 'scale(0.6)' },
-          '100%': { transform: 'scale(1)' },
-        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'fade-in': 'fade-in 0.3s ease-in-out',
-        'zoom-in': 'zoom-in 0.3s ease-in-out',
       },
     },
   },
